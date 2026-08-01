ad5787bda71bd3e2b965a2ff75f3739451bf79fb
Kris <lokiepandak@gmail.com>
2026-08-01 14:15:35 +0800

    Stage 1: signup route working

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from supabase import create_client, Client
+from pydantic import BaseModel
+from fastapi import HTTPException
+
+class AuthRequest(BaseModel):
+    email: str
+    password: str
 
 load_dotenv()
 
@@ -15,3 +21,19 @@ app = FastAPI()
 @app.on_event("startup")
 def startup_check():
     print("Server running and connected to Supabase")
+
+@app.post("/auth/signup")
+def sign_up(auth_request: AuthRequest):
+
+    if not auth_request.email or not auth_request.password:
+                raise HTTPException(status_code=400, detail="Email and password are required")
+    
+    try:
+        response = supabase.auth.sign_up({
+            "email": auth_request.email,
+            "password": auth_request.password
+        })
+        return {"message": "User signed up successfully", "user": response.user.email}
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
